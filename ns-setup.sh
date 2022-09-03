d968178b61cba392e70a859f9ff6429c1876b641
mo211073bkp <46835974+mo211073bkp@users.noreply.github.com>
2022-09-03 10:09:01 +0300

    Update ns-setup.sh

--- ns-setup.sh
+++ ns-setup.sh
@@ -34,7 +34,7 @@ read secret
 echo "NS_SECRET=$secret" >> .env
 
 curl https://raw.githubusercontent.com/mo211073bkp/test/main/docker-compose.yml --output docker-compose.yml
-sudo docker compose up -d
+sudo docker-compose up -d
 
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.11.1
