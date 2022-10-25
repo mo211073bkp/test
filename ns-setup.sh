1f9ee9fadc01584058cb9f6e6fed0736647d1804
mo211073bkp <46835974+mo211073bkp@users.noreply.github.com>
2022-10-26 00:42:30 +0300

    Update ns-setup.sh

--- ns-setup.sh
+++ ns-setup.sh
@@ -20,31 +20,18 @@ sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plu
 
 sudo docker run hello-world
 
-echo Итак, немного настроек
-echo
-echo Введите имя домена, на котором ваш Nightscout будет доступен:
+sudo docker volume create portainer_data
+docker pull portainer/portainer-ce:latest
+docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker ps
+
+echo Введите имя домена (sugarsveta.ru), на котором ваш Nightscout будет доступен:
 read domain
 echo "NS_DOMAIN=$domain" >> .env
 echo
 
-#secret=$(cat /proc/sys/kernel/random/uuid)
-echo
-echo Введите API secret key, 12 символов буквы и цифры:
-read secret
-echo "NS_SECRET=$secret" >> .env
-
-apt install nano
-
 curl https://raw.githubusercontent.com/mo211073bkp/test/main/docker-compose.yml --output docker-compose.yml
 sudo docker-compose up -d
 
-sudo docker volume create portainer_data
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.11.1
-sudo docker ps
-echo
-echo
-echo "Ваш секретный ключ для доступа к Nightscout (запишите!):"
-echo "secret: $secret"
-echo
-echo  "Адрес Вашего Nightscout"
+echo "Ваш Nightscout доступен по адресу (запишите!):"
 echo "domain: $domain"
