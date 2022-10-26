c4d2e0cf0a90362bfa25e2174559312453b3e341
mo211073bkp <46835974+mo211073bkp@users.noreply.github.com>
2022-10-26 08:17:59 +0300

    Update ns-setup.sh

--- ns-setup.sh
+++ ns-setup.sh
@@ -25,7 +25,7 @@ docker pull portainer/portainer-ce:latest
 docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 docker ps
 
-echo "Введите имя домена sugarsveta.ru, на котором ваш Nightscout будет доступен:"
+echo Введите имя домена sugarsveta.ru, на котором ваш Nightscout будет доступен:
 read domain
 echo "NS_DOMAIN=$domain" >> .env
 echo
