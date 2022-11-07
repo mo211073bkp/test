2afccf234e2e514fe7a60856b166a2c8bf203366
mo211073bkp <46835974+mo211073bkp@users.noreply.github.com>
2022-11-07 11:27:30 +0300

    Update ns-setup.sh

--- ns-setup.sh
+++ ns-setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+GREEN='\033[0;32m'
+NORMAL='\033[0m'
+YELLOW='\033[0;33m'
 
 sudo apt-get update
 
@@ -15,38 +18,44 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-#Устанавливаем Docker
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-#Проверяем как встал Docker
 sudo docker run hello-world
 
-#Устанавливаем Portainer последней версии
-sudo docker volume create portainer_data
-docker pull portainer/portainer-ce:latest
-docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-
-#Проверяем как встал Portainer
-docker ps
-
-echo "Введите имя домена sugarsveta.ru, на котором ваш Nightscout будет доступен:"
-read domain
-echo "NS_DOMAIN=$domain" >> .env
+echo Итак, немного настроек
 echo
 
-#echo
-#echo "Enter you email (the one SSL certificate will be generated for):"
-#echo "Введите ваш email (на него будет зарегистрирован SSL-сертификат):"
-#read email
-#echo "NS_EMAIL=$email" >> .env
-#echo
+while [[ ! "$domain" =~ ^([a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$ ]]; do
+  echo "Введите имя домена, на котором ваш Nightscout будет доступен:"
+  read domain
+done
 
-curl https://raw.githubusercontent.com/mo211073bkp/test/main/docker-compose.yml --output docker-compose.yml
-sudo docker-compose up -d
+echo "Ваш Nightscout будет зпрегистрирован по адресу (проверьте!):"
+echo "domain: $domain"
+read -p "Доменное имя введено без ошибок? (Нажмите [y/Y] для выполнения)"$'\n'  -n 1 -r
+echo -e "\n"
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+echo -e "$GREEN Запускаем скрипт установки $NORMAL"
+else
+echo -e "$YELLOW ОШИБКА!!! $NORMAL" 
+echo -e "$YELLOW Введите ПРАВИЛЬНО имя домена который Вы зарегистрировали $NORMAL"
+echo -e "$YELLOW (типа sugarsveta.ru), на котором ваш Nightscout будет доступен: $NORMAL"
+read domain
+echo "NS_DOMAIN=$domain"
+echo -e "$GREEN Ваш Nightscout будет зпрегистрирован по адресу: $NORMAL"
+echo "domain: $domain"
+fi
+echo "NS_DOMAIN=$domain" >> .env
 
-#Устанавливаем редактор Nano для возможности редактированя в будущем
-apt install nano
+secret=$(cat /proc/sys/kernel/random/uuid)
+echo "NS_SECRET=$secret" >> .env
 
-echo "Ваш Nightscout доступен по адресу (запишите!):"
+curl https://raw.githubusercontent.com/justmara/ns-setup/jino/docker-compose.yml --output docker-compose.yml
+
+sudo docker compose up -d
+
+echo "Ваш секретный ключ для доступа к Nightscout (запишите!):"
+echo "secret: $secret"
 echo "domain: $domain"
